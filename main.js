55ca45a9b2ad797db4403eb450c8cd08fb8dccb8
cesar <cesaroliva.work@gmail.com>
2025-09-25 15:35:12 -0600

    Terminando proyecto

--- main.js
+++ main.js
@@ -19,3 +19,26 @@ const swiper = new Swiper(".swiper", {
       },
     },
   });
+
+document.getElementById("beer-order-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let address = document.getElementById("address").value;
+    let beer = document.getElementById("beer-selection").value;
+    let quantity = document.getElementById("quantity").value;
+    let message = document.getElementById("message").value;
+
+    let texto = `🍻 *Nuevo Pedido* 🍻%0A
+1️⃣ Nombre: ${name}%0A
+2️⃣ Teléfono: ${phone}%0A
+3️⃣ Dirección: ${address}%0A
+4️⃣ Producto: ${beer}%0A
+5️⃣ Cantidad: ${quantity}%0A
+6️⃣ Mensaje: ${message ? message : "Ninguno"}`;
+
+    let url = `https://wa.me/528721186287?text=${texto}`;
+
+    window.open(url, "_blank");
+  });
